856bb28baf2265525caf81ea5224b4ed63f69816
pdiakumis <pdiakumis@unimelb.edu.au>
2020-03-22 01:43:57 +1100

    purple: process gene cnv

--- R/purple.R
+++ R/purple.R
@@ -7,14 +7,12 @@
 #' @param x Path to `purple.cnv.gene.tsv` file.
 #' @param v PURPLE version (default: 2.39). Used to determine the column names.
 #'
-#' @return The input file in a tibble.
-#'
+#' @return The input file as a tibble.
 #'
 #' @examples
 #' x <- system.file("extdata/purple/v2.39/purple.cnv.gene.tsv", package = "gpgr")
 #' p <- read_purple_cnv_gene(x, "2.39")
 #' p
-#'
 #'
 #' @testexamples
 #' expect_equal(colnames(p)[ncol(p)], "minMinorAllelePloidy")
@@ -23,7 +21,6 @@
 #' @export
 read_purple_cnv_gene <- function(x, v = "2.39") {
 
-  # Use utils::compareVersion(a, b) which gives -1/0/1, if a < b, a == b, a > b
   config <- function(v) {
     current <- list(
       nm = c("chromosome", "start", "end", "gene", "minCopyNumber", "maxCopyNumber",
@@ -36,9 +33,9 @@ read_purple_cnv_gene <- function(x, v = "2.39") {
       "2.39" = current,
       "2.45" = list(nm = c(current$nm, "foo", "bar"), type = paste0(current$type, "cc"))
     )
-    if (utils::compareVersion("2.45", v) <= 0) { # 2.45, 2.46...
+    if (numeric_version(v) >= numeric_version("2.45")) {
       return(l[["2.45"]])
-    } else if (utils::compareVersion("2.39", v) <= 0) { # 2.39, 2.40... 2.44
+    } else if (numeric_version(v) >= numeric_version("2.39")) {
       return(l[["2.39"]])
     } else {
       stop(glue::glue("You've specified PURPLE version {v}, which is older than 2.39.",
@@ -46,7 +43,67 @@ read_purple_cnv_gene <- function(x, v = "2.39") {
     }
   }
   conf <- config(v)
-  d <- readr::read_tsv(x, col_types = conf$type)
-  assertthat::assert_that(all(colnames(d) == conf$nm))
-  d
+  purple_cnv_gene <- readr::read_tsv(x, col_types = conf$type)
+  assertthat::assert_that(ncol(purple_cnv_gene) == length(conf$nm))
+  assertthat::assert_that(all(colnames(purple_cnv_gene) == conf$nm))
+  purple_cnv_gene
+}
+
+
+#' Process PURPLE CNV Gene File for UMCCRISE
+#'
+#' Processes the `purple.cnv.gene.tsv` file. Keeps genes that are in the
+#' [UMCCR cancer gene list](https://github.com/umccr/genes/blob/893a655801ce92715f05517b5052e4e81904e870/panels/umccr_2019-03-20.tsv)
+#' and selects columns of interest.
+#'
+#' @param x Path to `purple.cnv.gene.tsv` file.
+#' @param g Path to gene file containing at least three columns:
+#'        - `symbol`: gene name (character).
+#'        - `tumorsuppressor`: is this gene a tumor suppressor (logical).
+#'        - `oncogene`: is this gene an oncogene (logical).
+#' @param v PURPLE version (default: 2.39). Used to determine the column names.
+#'
+#' @return Tibble filtered to genes found in  `g`.
+#'
+#' @examples
+#' x <- system.file("extdata/purple/v2.39/purple.cnv.gene.tsv", package = "gpgr")
+#' g <- system.file("extdata/ref/umccr_cancer_genes_2019-03-20.tsv", package = "gpgr")
+#' pp <- process_purple_cnv_gene(x, g)
+#'
+#' @testexamples
+#' expect_equal(colnames(pp)[ncol(pp)], "minRegSupportStartEndMethod")
+#' expect_error(process_purple_cnv_gene(x, "2.38"))
+#'
+#' @export
+process_purple_cnv_gene <- function(x, g = NULL, v = "2.39") {
+  purple_cnv_gene <- read_purple_cnv_gene(x, v)
+  if (is.null(g)) {
+    g <- system.file("extdata/ref/umccr_cancer_genes_2019-03-20.tsv", package = "gpgr")
+  }
+  genes <- readr::read_tsv(g, col_types = readr::cols(symbol = "c", oncogene = "l", tumorsuppressor = "l")) %>%
+    dplyr::select(.data$symbol, .data$oncogene, .data$tumorsuppressor)
+  oncogenes <- genes %>% dplyr::filter(.data$oncogene) %>% dplyr::pull(.data$symbol)
+  tsgenes <- genes %>% dplyr::filter(.data$tumorsuppressor) %>% dplyr::pull(.data$symbol)
+
+  purple_cnv_gene %>%
+    dplyr::filter(.data$gene %in% genes$symbol) %>%
+    dplyr::mutate(
+      chromosome = as.factor(.data$chromosome),
+      transcriptID = paste0(.data$transcriptId, ".", .data$transcriptVersion),
+      minRegStartEnd = paste0(.data$minRegionStart, "-", .data$minRegionEnd),
+      minRegSupportStartEndMethod = paste0(.data$minRegionStartSupport, "-", .data$minRegionEndSupport,
+                                           " (", .data$minRegionMethod, ")"),
+      germDelReg = paste0(.data$germlineHomDeletionRegions, "/", .data$germlineHetToHomDeletionRegions),
+      oncogene = .data$gene %in% oncogenes,
+      tsgene = .data$gene %in% tsgenes,
+      onco_or_ts = dplyr::case_when(
+        .data$oncogene & .data$tsgene ~ "onco+ts",
+        .data$oncogene ~ "oncogene",
+        .data$tsgene ~ "tsgene",
+        TRUE ~ "")) %>%
+    dplyr::select(.data$gene, minCN = .data$minCopyNumber, maxCN = .data$maxCopyNumber,
+                  chrom = .data$chromosome, .data$start, .data$end,
+                  chrBand = .data$chromosomeBand, .data$onco_or_ts, .data$transcriptID, .data$minMinorAllelePloidy,
+                  somReg = .data$somaticRegions, .data$germDelReg, minReg = .data$minRegions,
+                  .data$minRegStartEnd, .data$minRegSupportStartEndMethod)
 }
