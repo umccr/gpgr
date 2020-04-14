5ad333d6e7a53539e0520634c1c6de26e68e181b
pdiakumis <pdiakumis@unimelb.edu.au>
2020-04-14 19:15:22 +1000

    refactor sv table generation --
    * Simplify split_double_col function
      - instead of complicating things with tidyeval, just
        melt, mutate, and recast into clean columns.
    * Create function for effect abbreviation
      - allows for testing
    * Clean up multiple invocations of split_double_col

--- R/sv.R
+++ R/sv.R
@@ -3,39 +3,40 @@
 #' Splits a column with 2 comma-separated fields into
 #' two columns.
 #'
-#' @param .data Input tibble data.
-#' @param col Column to split.
-#' @param is_pct Multiply by 100 (logical).
+#' @param d Input tibble data.
+#' @param nms A character vector of columns to split.
 #'
-#' @return A modified tibble with two columns.
+#' @return A modified tibble with the original columns averaged and split nicely.
 #'
 #' @examples
-#' x <- tibble::tibble(a = letters[1:10], b = paste0(round(runif(10), 2), ",", round(runif(10), 2)))
-#' (s <- gpgr:::split_double_col(x, b))
+#' x <- tibble::tibble(a = letters[1:11],
+#'                     b = c("0.4,0.8", paste0(round(runif(10), 2), ",", round(runif(10), 2))))
+#' (s <- gpgr:::split_double_col(x, "b"))
 #'
 #' @testexamples
-#' expect_equal(colnames(s), c("a", "b1", "b2", "b"))
-#' expect_error(gpgr:::split_double_col(x, c))
-#' expect_equal(unname(sapply(s, class)), c("character", "numeric", "numeric", "numeric"))
+#' expect_equal(colnames(s), "b")
+#' expect_equal(nrow(x), 11)
+#' expect_error(gpgr:::split_double_col(x, "c"))
+#' expect_equal(s$b[1], "0.6 (0.4, 0.8)")
 #'
-split_double_col <- function(.data, col, is_pct = FALSE) {
-  # - separate field into two parts
-  # - mutate to pct accordingly
-  # - original field is mean of two parts
-  f_q <- rlang::enquo(col)
-  f_str <- rlang::quo_name(f_q)
-  f1_str <- paste0(f_str, '1')
-  f2_str <- paste0(f_str, '2')
-  f1_q <- rlang::sym(f1_str)
-  f2_q <- rlang::sym(f2_str)
-  .data %>%
-    tidyr::separate(!!f_q, c(f1_str, f2_str), sep = ",", fill = "right") %>%
-    dplyr::mutate(
-      !!f1_q := round(as.double(!!f1_q) * ifelse(is_pct, 100, 1), 1),
-      !!f2_q := round(as.double(!!f2_q) * ifelse(is_pct, 100, 1), 1),
-      !!f_q  := round(((!!f1_q + ifelse(is.na(!!f2_q), !!f1_q, !!f2_q)) / 2), 1)
-    )
+split_double_col <- function(d, nms) {
+  assertthat::assert_that(inherits(d, "tbl_df"))
+  assertthat::assert_that(is.character(nms))
+
+  d %>%
+    dplyr::select(nms) %>%
+    dplyr::mutate(num = dplyr::row_number()) %>%
+    tidyr::pivot_longer(cols = nms, names_to = "col_nm", values_to = "x1_x2") %>%
+    tidyr::separate(.data$x1_x2, into = c("x1", "x2"), sep = ",", fill = "right") %>%
+    dplyr::mutate(x1 = round(as.double(.data$x1), 2),
+                  x2 = round(as.double(.data$x2), 2)) %>%
+    dplyr::mutate(avg = round(rowMeans(dplyr::select(., .data$x1, .data$x2), na.rm = TRUE), 2),
+                  out = as.character(glue::glue("{avg} ({x1}, {x2})"))) %>%
+    dplyr::select(.data$num, .data$col_nm, .data$out) %>%
+    tidyr::pivot_wider(names_from = "col_nm", values_from = "out") %>%
+    dplyr::select(-.data$num)
 }
+
 
 #' Count Number of Parts in a String
 #'
@@ -66,9 +67,62 @@ count_pieces <- function(x, sep) {
   ifelse(nchar(x) == 0, 0, stringr::str_count(x, sep) + 1)
 }
 
+#' Abbreviate SV Effects
+#'
+#' Abbreviates SV effects column.
+#'
+#'
+#' @param effects Input string with effects.
+#'
+#' @return An abbreviated string.
+#'
+#' @examples
+#' (e1 <- gpgr:::abbreviate_effect("3_prime_UTR_truncation&start_lost&splice_region_variant"))
+#' (e2 <- gpgr:::abbreviate_effect("duplication&gene_fusion&intron_variant"))
+#' (e3 <- gpgr:::abbreviate_effect("TF_binding_site_variant&TFBS_ablation"))
+#' (e4 <- gpgr:::abbreviate_effect("foo&bar&stop_gained&badaboom"))
+#'
+#' @testexamples
+#' expect_equal(e1, "3UTRtrunc, StartLoss, SpliceRegV")
+#' expect_equal(e2, "Dup, FusG, IntronV")
+#' expect_equal(e3, "TFBSVar, TFBSDel")
+#' expect_equal(e4, "foo, bar, StopGain, badaboom")
+#'
+abbreviate_effect <- function(effects) {
+
+  effect_abbreviations <- c(
+    "3_prime_UTR_truncation" = "3UTRtrunc", "3_prime_UTR_variant" = "3UTRvar",
+    "5_prime_UTR_truncation" = "5UTRtrunc",  "5_prime_UTR_variant" = "5UTRvar",
+    "feature_fusion" = "Fus", "bidirectional_gene_fusion" = "BidFusG", "gene_fusion" = "FusG",
+    "chromosome_number_variation" = "ChromNumV", "conservative_inframe_deletion" = "ConsInframeDel",
+    "downstream_gene_variant" = "DnstreamGV", "upstream_gene_variant" = "UpstreamGV",
+    "duplication" = "Dup", "exon_loss_variant" = "ExonLossV",
+    "feature_ablation" = "DelG", "transcript_ablation" = "DelTx",
+    "frameshift_variant" = "FrameshiftV", "intergenic_region" = "IntergenReg", "intragenic_variant" = "IntragenV",
+    "intron_variant" = "IntronV",  "no_func_effect" = "NoFuncEff", "no_prio_effect" = "NoPrioEff",
+    "non_coding_transcript_variant" = "NoncodTxV",
+    "splice_acceptor_variant" = "SpliceAccV",
+    "splice_donor_variant" = "SpliceDonV", "splice_region_variant" = "SpliceRegV",
+    "start_lost" = "StartLoss", "stop_gained" = "StopGain", "stop_lost" = "StopLoss",
+    "TF_binding_site_variant" = "TFBSVar",  "TFBS_ablation" = "TFBSDel")
+
+  effect_abbrev_nms <- names(effect_abbreviations)
+
+  # take string as x&y&z
+  # split by &
+  # abbreviate each piece and glue back with comma
+  .abbreviate_effect <- function(effect) {
+    ifelse(effect %in% effect_abbrev_nms, effect_abbreviations[effect], effect)
+  }
+
+  strsplit(effects, "&")[[1]] %>%
+    purrr::map_chr(.abbreviate_effect) %>%
+    paste(collapse = ", ")
+}
+
 #' Read SV TSV
 #'
-#' Reads the TSV file output by umccrise.
+#' Reads the Manta TSV file output by umccrise.
 #'
 #' @param x Path to `manta.tsv` output by umccrise.
 #' @param v umccrise version (default: 0.18). Used to verify the column names.
@@ -115,117 +169,113 @@ read_sv_tsv <- function(x, v = "0.18") {
   somatic_sv_tsv
 }
 
-effect_abbreviations <- c(
-  "3_prime_UTR_truncation" = "3UTRtrunc", "3_prime_UTR_variant" = "3UTRvar",
-  "5_prime_UTR_truncation" = "5UTRtrunc",  "5_prime_UTR_variant" = "5UTRvar",
-  "feature_fusion" = "Fus", "bidirectional_gene_fusion" = "BidFusG", "gene_fusion" = "FusG",
-  "chromosome_number_variation" = "ChromNumV", "conservative_inframe_deletion" = "ConsInframeDel",
-  "downstream_gene_variant" = "DnstreamGV", "upstream_gene_variant" = "UpstreamGV",
-  "duplication" = "Dup", "exon_loss_variant" = "ExonLossV",
-  "feature_ablation" = "DelG", "transcript_ablation" = "DelTx",
-  "frameshift_variant" = "FrameshiftV", "intergenic_region" = "IntergenReg", "intragenic_variant" = "IntragenV",
-  "intron_variant" = "IntronV",  "no_func_effect" = "NoFuncEff", "no_prio_effect" = "NoPrioEff",
-  "non_coding_transcript_variant" = "NoncodTxV",
-  "splice_acceptor_variant" = "SpliceAccV",
-  "splice_donor_variant" = "SpliceDonV", "splice_region_variant" = "SpliceRegV",
-  "start_lost" = "StartLoss", "stop_gained" = "StopGain", "stop_lost" = "StopLoss",
-  "TF_binding_site_variant" = "TFBSVar",  "TFBS_ablation" = "TFBSDel")
-effect_abbrev_nms <- names(effect_abbreviations)
+#' Process Structural Variants
+#'
+#' Processes the Manta TSV file output by umccrise.
+#'
+#' @param x Path to `manta.tsv` output by umccrise.
+#' @param v umccrise version (default: 0.18). Used to verify the column names.
+#'
+#' @return A list with melted/unmelted tibbles.
+#'
+#' @examples
+#' x <- system.file("extdata/umccrise/v0.18/sv/manta.tsv", package = "gpgr")
+#' (sv <- process_sv(x))
+#'
+#' @testexamples
+#' expect_true(inherits(sv, "list"))
+#' expect_equal(length(sv), 2)
+#' expect_equal(names(sv), c("unmelted", "melted"))
+#'
+#' @export
+process_sv <- function(x, v = "0.18") {
+  sv <- read_sv_tsv(x, v)
+  unmelted <- NULL
+  melted <- NULL
 
-abbreviate_effect <- function(effects) {
-  # take string as x&y&z
-  # split by &
-  # abbreviate each piece and glue back with comma
-
-  .abbreviate_effect <- function(effect) {
-    ifelse(effect %in% effect_abbrev_nms, effect_abbreviations[effect], effect)
+  if (!nrow(sv) > 0) {
+    return(list(
+      unmelted = NULL,
+      melted = NULL
+    ))
   }
 
-  strsplit(effects, "&")[[1]] %>%
-    purrr::map_chr(.abbreviate_effect) %>%
-    paste(collapse = ", ")
-}
-abbreviate_effectv <- Vectorize(abbreviate_effect)
-
-sv_path <- params$somatic_sv
-sv_unmelted <- NULL
-sv_all <- NULL
-
-if (length(readLines(con = sv_path, n = 2)) > 1) {
-  somatic_sv_tsv <- readr::read_tsv(sv_path, col_names = TRUE, col_types = "ccciicccccicccccdciicc")
-  sv_unmelted <- somatic_sv_tsv %>%
-    dplyr::select(-caller, -sample) %>%
-    split_double_col(AF_BPI) %>%
-    split_double_col(AF_PURPLE) %>%
-    split_double_col(CN_PURPLE) %>%
-    split_double_col(CN_change_PURPLE) %>%
+  cols_to_split <- c("AF_BPI", "AF_PURPLE", "CN_PURPLE", "CN_change_PURPLE")
+  double_cols <- split_double_col(sv, cols_to_split)
+  unmelted <- sv %>%
+    dplyr::select(-c(cols_to_split, "caller", "sample")) %>%
+    dplyr::bind_cols(double_cols) %>%
+    tidyr::separate(.data$split_read_support, c("SR_ref", "SR_alt"), ",", convert = TRUE) %>%
+    tidyr::separate(.data$paired_support_PR, c("PR_ref", "PR_alt"), ",", convert = TRUE) %>%
     dplyr::mutate(
-      AF_BPI = ifelse(is.na(AF_BPI), NA, paste0(AF_BPI, " (", AF_BPI1, ",", AF_BPI2, ")")),
-      AF_PURPLE = ifelse(is.na(AF_PURPLE), NA, paste0(AF_PURPLE, " (", AF_PURPLE1, ", ", AF_PURPLE2, ")")),
-      CN_PURPLE = ifelse(is.na(CN_PURPLE), NA, paste0(CN_PURPLE, " (", CN_PURPLE1, ", ", CN_PURPLE2, ")")),
-      CN_change_PURPLE = ifelse(is.na(CN_change_PURPLE), NA, paste0(CN_change_PURPLE, " (", CN_change_PURPLE1, ", ", CN_change_PURPLE2, ")"))
-    ) %>%
-    tidyr::separate(split_read_support, c("SR_ref", "SR_alt"), ",", convert = TRUE) %>%
-    tidyr::separate(paired_support_PR, c("PR_ref", "PR_alt"), ",", convert = TRUE) %>%
-    dplyr::mutate(
-      SR_PR_alt = paste0(SR_alt, ",", PR_alt),
-      SR_PR_ref = paste0(SR_ref, ",", PR_ref),
-      Ploidy = round(as.double(Ploidy_PURPLE), 2),
-      chrom = sub("chr", "", chrom),
-      chrom = as.factor(chrom), # for better DT filtering
-      svtype = ifelse(is.na(PURPLE_status), svtype, "PURPLE_inf"),
-      Start = ifelse(is.na(PURPLE_status), START_BPI, start),
-      nann = count_pieces(annotation, ","),
-      varnum = dplyr::row_number(),
-      varnum = sprintf(glue::glue("%0{nchar(nrow(.))}d"), varnum))
+      SR_PR_alt = paste0(.data$SR_alt, ",", .data$PR_alt),
+      SR_PR_ref = paste0(.data$SR_ref, ",", .data$PR_ref),
+      Ploidy = round(as.double(.data$Ploidy_PURPLE), 2),
+      # chrom = sub("chr", "", .data$chrom),
+      # chrom = as.factor(.data$chrom), # for better DT filtering
+      svtype = ifelse(is.na(.data$PURPLE_status), .data$svtype, "PURPLE_inf"),
+      Start = ifelse(is.na(.data$PURPLE_status), .data$START_BPI, .data$start),
+      nann = count_pieces(.data$annotation, ","),
+      vcfnum = dplyr::row_number(),
+      vcfnum = sprintf(glue::glue("%0{nchar(nrow(.))}d"), .data$vcfnum))
 
-  # Fix BND IDs
-  sv_unmelted_bnd <- sv_unmelted %>%
-    dplyr::filter(svtype == "BND") %>%
-    tidyr::separate(ID, into = c("BND_group", "BND_mate"), sep = -1, convert = TRUE, remove = FALSE) %>%
-    dplyr::group_by(BND_group) %>%
+  # BND IDs
+  # Two BND mates share the same ID up to the last digit (0 or 1)
+  unmelted_bnd <- unmelted %>%
+    dplyr::filter(.data$svtype == "BND") %>%
+    tidyr::separate(.data$ID, into = c("BND_group", "BND_mate"), sep = -1, convert = TRUE, remove = FALSE) %>%
+    dplyr::group_by(.data$BND_group) %>%
     dplyr::mutate(
+      # index per group 1, 2, 3..
       BND_ID = dplyr::group_indices(),
-      BND_ID = sprintf(glue::glue("%0{nchar(nrow(.))}d"), BND_ID),
-      BND_mate = ifelse(BND_mate == 0, "A", "B")) %>%
+      # turns into 001, 002, 003... if you've got 100+ rows
+      BND_ID = sprintf(glue::glue("%0{nchar(nrow(.))}d"), .data$BND_ID),
+      BND_mate = ifelse(.data$BND_mate == 0, "A", "B")) %>%
     dplyr::ungroup() %>%
     dplyr::bind_cols(., .[match(.$ID, .$MATEID), "chrom"]) %>%
-    dplyr::rename(BND_mate_chrom = chrom1)
+    dplyr::rename(BND_mate_chrom = .data$chrom1)
 
-  sv_unmelted_other <- sv_unmelted %>%
-    dplyr::filter(svtype != "BND")
+  unmelted_other <- unmelted %>%
+    dplyr::filter(.data$svtype != "BND")
 
-  sv_unmelted <-
-    dplyr::bind_rows(sv_unmelted_bnd,
-                     sv_unmelted_other) %>%
+  unmelted_all <-
+    dplyr::bind_rows(unmelted_bnd,
+                     unmelted_other) %>%
     dplyr::mutate(
-      END_BPI = base::format(END_BPI, big.mark = ",", trim = TRUE),
-      End = ifelse(svtype == "BND", paste0(BND_mate_chrom, ":", END_BPI), END_BPI)) %>%
-    dplyr::select(varnum, TierTop = tier,
-                  Chr = chrom, Start, End,
-                  Type = svtype,
-                  ID, MATEID, BND_ID, BND_mate,
-                  SR_PR_alt, SR_PR_ref, Ploidy,
-                  AF_PURPLE, AF_BPI,
-                  CNC = CN_change_PURPLE, CN = CN_PURPLE,
-                  SScore = somaticscore, nann, annotation)
+      END_BPI = base::format(.data$END_BPI, big.mark = ",", trim = TRUE),
+      End = paste0(
+        ifelse(.data$svtype == "BND", .data$BND_mate_chrom, .data$chrom),
+        ":",
+        .data$END_BPI)) %>%
+    dplyr::select(.data$vcfnum, TierTop = .data$tier,
+                  Chr = .data$chrom, .data$Start, .data$End,
+                  Type = .data$svtype,
+                  .data$ID, .data$MATEID, .data$BND_ID, .data$BND_mate,
+                  .data$SR_PR_alt, .data$SR_PR_ref, .data$Ploidy,
+                  .data$AF_PURPLE, .data$AF_BPI,
+                  CNC = .data$CN_change_PURPLE, CN = .data$CN_PURPLE,
+                  SScore = .data$somaticscore, .data$nann, .data$annotation)
 
-  sv_all <- sv_unmelted %>%
-    dplyr::mutate(annotation = strsplit(annotation, ',')) %>%
-    tidyr::unnest(annotation) %>%
-    tidyr::separate(
-      annotation, c('Event', 'Effect', 'Genes', 'Transcript', 'Detail', 'Tier'),
-      sep = '\\|', convert = FALSE) %>%
-    dplyr::mutate(
-      ntrx = count_pieces(Transcript, "&"),
-      ngen = count_pieces(Genes, "&"),
-      neff = count_pieces(Effect, "&"),
-      Transcript = Transcript %>% stringr::str_replace_all('&', ', '),
-      Genes = Genes %>% stringr::str_replace_all('&', ', '),
-      Effect = abbreviate_effectv(Effect)) %>%
-    dplyr::distinct() %>%
-    dplyr::arrange(Tier, Genes, Effect)
-} else {
-  sv_unmelted <- tibble(WARNING = "THERE WERE 0 SVs PRIORITISED!!")
-  sv_all <- tibble(WARNING = "THERE WERE 0 SVs PRIORITISED!!")
+  abbreviate_effectv <- Vectorize(abbreviate_effect)
+
+  melted <- unmelted_all %>%
+      dplyr::mutate(annotation = strsplit(.data$annotation, ',')) %>%
+      tidyr::unnest(.data$annotation) %>%
+      tidyr::separate(
+        .data$annotation, c('Event', 'Effect', 'Genes', 'Transcript', 'Detail', 'Tier'),
+        sep = '\\|', convert = FALSE) %>%
+      dplyr::mutate(
+        ntrx = count_pieces(.data$Transcript, "&"),
+        ngen = count_pieces(.data$Genes, "&"),
+        neff = count_pieces(.data$Effect, "&"),
+        Transcript = .data$Transcript %>% stringr::str_replace_all('&', ', '),
+        Genes = .data$Genes %>% stringr::str_replace_all('&', ', '),
+        Effect = abbreviate_effectv(.data$Effect)) %>%
+      dplyr::distinct() %>%
+      dplyr::arrange(.data$Tier, .data$Genes, .data$Effect)
+
+  list(
+    unmelted = unmelted,
+    melted = melted
+  )
 }
