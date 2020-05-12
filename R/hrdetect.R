7b155d61172c264917872b90bfbae5f6d54f9e78
pdiakumis <pdiakumis@unimelb.edu.au>
2020-05-12 16:14:39 +1000

    hrdetect: generate sv catalogue

--- R/hrdetect.R
+++ R/hrdetect.R
@@ -32,3 +32,47 @@ hrdetect_read_snvindel_vcf <- function(x) {
   )
 }
 
+#' Read VCF with SVs for use with HRDetect
+#'
+#' Reads a VCF with SVs for use with HRDetect.
+#'
+#' @param x Path to VCF.
+#' @param nm Sample name.
+#' @param hg_version Human genome version (default: hg38).
+#'
+#' @return Tibble with following BEDPE-like columns:
+#' - chrom1
+#' - start1
+#' - end1
+#' - chrom2
+#' - start2
+#' - end2
+#' - sample
+#' - strand1
+#' - strand2
+#'
+#'
+#' @examples
+#' x <- system.file("extdata/umccrise/v0.18/sv/manta.vcf.gz", package = "gpgr")
+#' sv_bedpe <- hrdetect_read_sv_vcf(x, nm = "SAMPLE")
+#' head(sv_bedpe)
+#'
+#' @testexamples
+#' expect_equal(nrow(sv_bedpe), 190)
+#' expect_equal(colnames(sv_bedpe), c("chrom1", "start1", "end1", "chrom2",
+#'              "start2", "end2", "sample", "strand1", "strand2"))
+#' @export
+hrdetect_read_sv_vcf <- function(x, nm = NULL, hg_version = "hg38") {
+
+  assertthat::assert_that(!is.null(nm))
+
+  vcf <- VariantAnnotation::readVcf(x, hg_version)
+  gr <- StructuralVariantAnnotation::breakpointRanges(vcf)
+  bedpe <- StructuralVariantAnnotation::breakpointgr2bedpe(gr) %>%
+    dplyr::mutate_if(is.factor, as.character) %>%
+    dplyr::mutate(sample = nm) %>%
+    dplyr::select(.data$chrom1:.data$end2, .data$sample, .data$strand1, .data$strand2)
+
+  bedpe
+
+}
