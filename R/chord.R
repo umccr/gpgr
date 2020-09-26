b1482a9b3ca72867b3af79f31207aeff8923333f
pdiakumis <pdiakumis@unimelb.edu.au>
2020-09-26 23:55:01 +1000

    add chord_mantavcf2df

--- R/chord.R
+++ R/chord.R
@@ -3,40 +3,48 @@
 #' Runs CHORD given SNV and SV VCF files. **NOTE**: make sure you have
 #' the BSgenome.Hsapiens.UCSC.hgXX installed.
 #'
-#' @param snv Path to SNV VCF.
-#' @param sv Path to SV VCF.
-#' @param sample Name of sample to use.
-#' @param genome Human genome assembly. One of 'hg38' (default) or 'hg19'.
+#' @param vcf.snv Path to VCF containing SNVs and INDELs.
+#' @param vcf.sv Path to VCF containing SVs.
+#' @param df.sv A data.frame object containing the columns
+#'        'SVTYPE' and 'SVLEN' from a Manta SV VCF.
+#' @param sample.name Name of sample to use.
+#' @param ref.genome Human genome assembly. One of 'hg38' (default), 'hg19' or 'GRCh37'.
+#' @param sv.caller manta (default) or gridss.
+#' @param ... Other arguments to be passed to `CHORD::extractSigsChord()`.
 #'
 #' @examples
 #' \dontrun{
 #' snv <- system.file("extdata/umccrise/v0.18/snv/somatic-ensemble-PASS.vcf.gz", package = "gpgr")
 #' sv <- system.file("extdata/umccrise/v0.18/sv/manta.vcf.gz", package = "gpgr")
-#' chord_res <- run_chord(snv = snv, sv = sv, sample = "foo")
+#' chord_res <- run_chord(vcf.snv = snv, vcf.sv = sv, sample.name = "foo")
+#' chord_res2 <- run_chord(vcf.snv = snv, df.sv = gpgr:::chord_mantavcf2df(sv), sample.name = "foo")
 #' }
 #'
 #'
 #' @return List with extracted signatures and HRD prediction.
 #'
 #' @export
-run_chord <- function(snv, sv, sample, genome = "hg38") {
-  assertthat::assert_that(all(file.exists(c(snv, sv))))
-  assertthat::assert_that(genome %in% c("hg19", "hg38", "GRCh37"))
-  ref_genome <- NULL
-  if (genome == "hg38") {
-    ref_genome <- BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38
-  } else if (genome == "hg19") {
-    ref_genome <- BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19
-  } else if (genome == "GRCh37") {
-    ref_genome <- BSgenome.Hsapiens.1000genomes.hs37d5::BSgenome.Hsapiens.1000genomes.hs37d5
+run_chord <- function(vcf.snv = NULL, vcf.sv = NULL, df.sv = NULL, sample.name = NULL, ref.genome = "hg38", sv.caller = "manta", ...) {
+  avail_genomes <- c("hg19", "hg38", "GRCh37")
+  g <- NULL
+  if (ref.genome == "hg38") {
+    g <- BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38
+  } else if (ref.genome == "hg19") {
+    g <- BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19
+  } else if (ref.genome == "GRCh37") {
+    g <- BSgenome.Hsapiens.1000genomes.hs37d5::BSgenome.Hsapiens.1000genomes.hs37d5
+  } else {
+    stop(glue::glue("ref.genome should be one of {paste(avail_genomes, collapse = ', ')}"))
   }
 
   contexts <- CHORD::extractSigsChord(
-    vcf.snv = snv,
-    vcf.sv = sv,
-    sv.caller='manta',
-    sample.name = sample,
-    ref.genome = ref_genome
+    vcf.snv = vcf.snv,
+    vcf.sv = vcf.sv,
+    df.sv = df.sv,
+    sv.caller = sv.caller,
+    sample.name = sample.name,
+    ref.genome = g,
+    ...
   )
 
   prediction <- CHORD::chordPredict(features = contexts,
@@ -47,5 +55,10 @@ run_chord <- function(snv, sv, sample, genome = "hg38") {
     contexts = contexts,
     prediction = prediction
   )
+}
 
+chord_mantavcf2df <- function(in_vcf) {
+  d <- bedr::read.vcf(in_vcf, split.info = TRUE, verbose = FALSE)
+  tibble::tibble(sv_type = d$vcf$SVTYPE,
+                 sv_len = d$vcf$SVLEN)
 }
