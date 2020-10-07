7dab6827e733b1dee897535406574070afc8fb69
pdiakumis <pdiakumis@unimelb.edu.au>
2020-10-07 19:47:00 +1100

    decrease example line length to satisfy check

--- R/chord.R
+++ R/chord.R
@@ -16,8 +16,8 @@
 #'
 #' snv <- system.file("extdata/umccrise/snv/somatic-ensemble-PASS.vcf.gz", package = "gpgr")
 #' sv <- system.file("extdata/umccrise/sv/manta.vcf.gz", package = "gpgr")
-#' # chord_res <- run_chord(vcf.snv = snv, vcf.sv = sv, sample.name = "foo")
-#' chord_res <- run_chord(vcf.snv = snv, df.sv = gpgr:::chord_mantavcf2df(sv), sample.name = "foo") # a bit faster
+#' chord_res <- run_chord(vcf.snv = snv, df.sv = gpgr:::chord_mantavcf2df(sv), sample.name = "foo")
+#' # chord_res <- run_chord(vcf.snv = snv, vcf.sv = sv, sample.name = "foo") # a bit slower
 #'
 #' @testexamples
 #'
--- tests/testthat/test-roxytest-testexamples-chord.R
+++ tests/testthat/test-roxytest-testexamples-chord.R
@@ -7,8 +7,8 @@ test_that("Function run_chord() @ L32", {
   
   snv <- system.file("extdata/umccrise/snv/somatic-ensemble-PASS.vcf.gz", package = "gpgr")
   sv <- system.file("extdata/umccrise/sv/manta.vcf.gz", package = "gpgr")
-  # chord_res <- run_chord(vcf.snv = snv, vcf.sv = sv, sample.name = "foo")
-  chord_res <- run_chord(vcf.snv = snv, df.sv = gpgr:::chord_mantavcf2df(sv), sample.name = "foo") # a bit faster
+  chord_res <- run_chord(vcf.snv = snv, df.sv = gpgr:::chord_mantavcf2df(sv), sample.name = "foo")
+  # chord_res <- run_chord(vcf.snv = snv, vcf.sv = sv, sample.name = "foo") # a bit slower
   
   expect_equal(length(chord_res), 2)
   expect_equal(names(chord_res), c("contexts", "prediction"))
