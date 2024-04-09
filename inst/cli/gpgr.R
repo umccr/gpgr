65fd293c506bb16d12b068f75b615009177ce164
pdiakumis <peterdiakumis@gmail.com>
2024-04-09 12:23:54 +1000

    linx functionality moved to umccr/linxreport

--- inst/cli/gpgr.R
+++ inst/cli/gpgr.R
@@ -12,19 +12,15 @@ p$add_argument("-v", "--version", action = "version", version = glue::glue("{pro
 subparser_name <- "subparser_name"
 subp <- p$add_subparsers(help = "sub-command help", dest = subparser_name)
 
-source(system.file("cli/linx.R", package = "gpgr"))
 source(system.file("cli/canrep.R", package = "gpgr"))
 
-linx_add_args(subp)
 canrep_add_args(subp)
 
 args <- p$parse_args()
 if (length(args$subparser_name) == 0) {
   p$print_help()
-} else if (args$subparser_name == "linx") {
-  linx_parse_args(args)
 } else if (args$subparser_name == "canrep") {
   canrep_parse_args(args)
 } else {
-  stop("Need to specify 'linx' or 'canrep' in the cli...")
+  stop("Need to specify 'canrep' in the cli...")
 }
